c6327a74acce2552e19b9755152373600438ea72
Borys35 <borys@Borys.>
2025-03-17 14:16:21 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/fingers_detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fingers_detection.dir/main.cpp.o"
+  "CMakeFiles/fingers_detection.dir/main.cpp.o.d"
+  "fingers_detection"
+  "fingers_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fingers_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/fingers_detection.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fingers_detection.
+# This may be replaced when dependencies are built.
