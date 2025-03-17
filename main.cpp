1c9cd0f6241bc1ef65b9bb472b290dc67dc5d297
Borys35 <borys@Borys.>
2025-03-17 14:24:01 +0100

    display video

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <filesystem>
 #include <iostream>
+#include <vector>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+#include <opencv4/opencv2/core/mat.hpp>
+#include <opencv4/opencv2/core/types.hpp>
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv4/opencv2/core/matx.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string path = "../resources/hand_only.MP4";
+    cv::VideoCapture cap(path);
+
+    while (true) {
+        cv::Mat frame;
+        cap.read(frame);
+
+        cv::imshow("image", frame);
+        if (cv::waitKey(30) == 27)
+            return 0;
+    }
+
     return 0;
 }
